1974bc52a8417b8e21cea772ebe5b116ba377b1b
je0nh <winter0pear@gmail.com>
2025-03-07 00:35:56 +0900

    array 예제

---
+++ src/main/java/oop/reference/ArrayExample.java
@@ -0,0 +1,9 @@
+package oop.reference;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        // 값을 목록으로 배열 생성
+        int[] intArray = {1, 2, 3, 4, 5};
+        System.out.println(intArray[3]);
+    }
+}
