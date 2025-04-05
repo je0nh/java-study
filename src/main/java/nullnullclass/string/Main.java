105d423eeb36e21f271d4cc2bbaedf4923ccb1dc
je0nh <winter0pear@gmail.com>
2025-04-05 18:21:03 +0900

    정답 코드

--- src/main/java/nullnullclass/string/Main.java
+++ src/main/java/nullnullclass/string/Main.java
@@ -4,55 +4,26 @@ class MyString {
     private byte[] string;
 
     public String getString() {
-        if (this.string == null) return "(null)";
-
-        String s = "";
-
-        int n = string.length / 2;
-        for (int i = 0; i < n; i++) {
-            byte high = this.string[i * 2];
-            byte low = this.string[(i * 2) + 1];
-
-            char c = (char) ((high << 8) | (low & 0xFF));
-            s += c;
-        }
-        return s;
+        if (this.string == null || this.string.length == 0) return "(null)";
+        return new String(string);
     }
 
     public void setString(String s) {
-        int n = s.length();
-        this.string = new byte[n * 2];
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            byte high = (byte) (c >> 8);
-            byte low = (byte) (c & 0xFF);
-
-            string[i * 2] = high;
-            string[(i * 2) + 1] = low;
-        }
+        this.string = s.getBytes();
     }
 
     public int length() {
         if (this.string == null) {
             return 0;
         }
-        return string.length / 2;
+        return string.length;
     }
 
     public int compareTo(String s) {
-        int n = string.length / 2;
-        if (s.length() != n) return -1;
-
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            byte high = (byte) (c >> 8);
-            byte low = (byte) (c & 0xFF);
-
-            if (string[i * 2] != high || string[(i * 2) + 1] != low) {
-                return -1;
-            }
+        if (this.string == null) {
+            return -1;
         }
-        return 0;
+        return s.compareTo(new String(string));
     }
 }
 
