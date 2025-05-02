f41cc32c0f40d096dd42ebe9ec833b5d6fb55bd8
jeonh <jhlee@ncdata.co.kr>
2025-05-02 16:41:02 +0900

    static 연습 예제

---
+++ src/main/java/nullnullclass/copy/StaticExample.java
@@ -0,0 +1,37 @@
+package nullnullclass.copy;
+
+public class StaticExample {
+    private byte[] string;
+    private static int count = 0;
+
+    private StaticExample() {
+        string = null;
+    }
+
+    private StaticExample(String str) {
+        this.string = str.getBytes();
+        count++;
+    }
+
+    private StaticExample(int num) {
+        String tmp = String.format("%d", num);
+        this.string = tmp.getBytes();
+        count++;
+    }
+
+    public static StaticExample newString(String str) {
+        return new StaticExample(str);
+    }
+
+    public static StaticExample newString(int num) {
+        return new StaticExample(num);
+    }
+
+    public String getString() {
+        return new String(string);
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
